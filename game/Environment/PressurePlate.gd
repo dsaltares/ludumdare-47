8518a08c9a55cd3c49d0376e10e25d21353fcf6f
Jay Cano <inaki.cano@gmail.com>
2020-10-03 16:59:43 +0100

    add door and connect to pressure plate

--- game/Environment/PressurePlate.gd
+++ game/Environment/PressurePlate.gd
@@ -1,5 +1,8 @@
 extends Spatial
 
+signal pressure_plate_activated
+signal pressure_plate_deactivated
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -10,6 +13,12 @@ func _ready():
 
 func _on_Area_body_entered(body):
 	$AnimationPlayer.play("Activation")
+	emit_signal("pressure_plate_activated")
 
 func _on_Area_body_exited(body):
 	$AnimationPlayer.play_backwards("Activation")
+	emit_signal("pressure_plate_deactivated")
+
+
+func _on_pressure_plate_activated():
+	pass # Replace with function body.
