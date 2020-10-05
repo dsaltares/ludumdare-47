18ac02ac53cdce8820d8d67bdd442fd39ea5f29b
Jay Cano <inaki.cano@gmail.com>
2020-10-05 22:15:22 +0100

    add controller detection to the tutorial

--- game/Screens/Tutorial1.gd
+++ game/Screens/Tutorial1.gd
@@ -1,6 +1,22 @@
 extends Label
 
+onready var ps4_controls := $PS4
+onready var xbox_controls := $Xbox
+onready var pc_controls := $PC
+
 var showing := false
+
+enum ControlType {
+	PC,
+	PS4,
+	XBOX,
+}
+
+func _ready():
+	var control_type = _get_control_type()
+	pc_controls.visible = control_type == ControlType.PC
+	ps4_controls.visible = control_type == ControlType.PS4
+	xbox_controls.visible = control_type == ControlType.XBOX
 
 func _on_timeout():
 	showing = false
@@ -11,3 +27,19 @@ func _on_first_button_pressed(body : KinematicBody):
 		show()
 		showing = true
 		$Timer.start()
+
+func _get_control_type():
+	var has_joypad := Input.get_connected_joypads().size() > 0
+	if not has_joypad:
+		return ControlType.PC
+	var joypad_name = Input.get_joy_name(0).to_lower()
+	var names = [
+		"dualshock",
+		"ps4",
+		"sony",
+		"playstation"
+	]
+	for name in names:
+		if joypad_name.find(name) != -1:
+			return ControlType.PS4
+	return ControlType.XBOX
