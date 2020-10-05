9e8a5976ca55557e2db7b5806dcbb6ccd749d7b7
Jay Cano <inaki.cano@gmail.com>
2020-10-05 19:13:37 +0100

    add body parameter to button event handlers

--- game/Environment/Door.gd
+++ game/Environment/Door.gd
@@ -1,8 +1,8 @@
 extends StaticBody
 
 
-func _on_pressure_plate_activated():
+func _on_pressure_plate_activated(body):
 	$AnimationPlayer.play("Open")
 
-func _on_pressure_plate_deactivated():
+func _on_pressure_plate_deactivated(body):
 	$AnimationPlayer.play("Close")
