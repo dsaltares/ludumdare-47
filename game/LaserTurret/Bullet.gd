cda078790c1852da9694174dc005656b99baa709
David Saltares <david.saltares@gmail.com>
2020-10-05 18:50:01 +0300

    ability to disable turret and bullet

--- game/LaserTurret/Bullet.gd
+++ game/LaserTurret/Bullet.gd
@@ -1,14 +1,14 @@
 extends Area
 class_name Bullet
 
-const SPEED = 15.0
+export var speed := 15.0
 
 onready var BulletHitEffectScene := preload("res://LaserTurret/BulletHitEffect.tscn")
 
 var bodies_to_ignore = []
 
 func _physics_process(delta: float) -> void:
-	global_transform.origin += get_global_transform().basis.x * SPEED * delta
+	global_transform.origin += get_global_transform().basis.x * speed * delta
 
 func _on_Bullet_body_entered(body: Node) -> void:
 	if body in bodies_to_ignore:
