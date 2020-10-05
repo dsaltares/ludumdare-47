ca8cdfcd3a68da7c7f67824d407db0e5a245fc2b
Jay Cano <inaki.cano@gmail.com>
2020-10-05 09:42:02 +0100

    expose timer variables for spikes and turrets

--- game/LaserTurret/LaserTurret.gd
+++ game/LaserTurret/LaserTurret.gd
@@ -1,12 +1,16 @@
 extends Spatial
 
+export var shooting_rate := 2.0
+
 onready var BulletScene := preload("res://LaserTurret/Bullet.tscn")
 onready var shoot_timer := $ShootTimer
 onready var shooting_position := $Graphics/ShootingPosition
 onready var laser_sfx := $LaserSFX
 
 func _ready() -> void:
+	shoot_timer.set("wait_time", shooting_rate)
 	shoot_timer.connect("timeout", self, "shoot")
+	shoot_timer.start()
 	
 func shoot() -> void:
 	var bullet = BulletScene.instance()
